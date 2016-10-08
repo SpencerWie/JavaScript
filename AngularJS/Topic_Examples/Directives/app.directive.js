88937c33a36374dd8b1c71dea1eb4648155d77b9
SpencerWie <spdwiecz@iupui.edu>
2016-10-07 23:47:37 -0400

    Update Directives, added more examples

--- AngularJS/Topic_Examples/Directives/app.directive.js
+++ AngularJS/Topic_Examples/Directives/app.directive.js
@@ -1,12 +1,31 @@
 var module = angular.module('app');
 
-module.directive('helloworld',  function(){
+/*
+Directive Information:
+
+---Restrictions ---
+	'E' => Element
+	'A' => Attribute
+	'C' => Class
+	'M' => HTML Comment
+*/
+
+// This is a custome made HTML element
+module.directive('helloE',  function(){
 	
 	var helloworld = {};
 	
 	helloworld.restrict = 'E';                         // Restrict this diretive to be only a HTML Element
-	helloworld.template = "<h2>Hello World</h2>"       // A Template is a string or HTML file given and renders as HTML
+	helloworld.template = "<h2>Hello World!</h2>";     // A Template is a string or HTML file given and renders as HTML
 	
 	return helloworld;
+});
+
+module.directive('helloA',  function(){
+	console.log('ran helloA')
+	return {
+		restrict: "A",
+		link: function(){ alert("Hello World!"); }
+	}
 });
 
