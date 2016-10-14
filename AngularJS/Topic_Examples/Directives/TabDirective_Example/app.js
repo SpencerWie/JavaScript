ce372dcaa8a7e05db66bde1eec80c7e566382922
SpencerWie <spdwiecz@iupui.edu>
2016-10-14 00:50:35 -0400

    Added basic examples of containers/component concepts

--- AngularJS/Topic_Examples/Directives/TabDirective_Example/app.js
+++ AngularJS/Topic_Examples/Directives/TabDirective_Example/app.js
@@ -4,9 +4,15 @@ angular.module('app', [])
   return {
     restrict: 'E',
     transclude: true,
-    template: '<h2>Hello world!</h2> <div role="tabpanel" ng-transclude></div>',
-    scope: { },
-    link: function(scope, elem, attr) {}
+    template: '<div role="tabpanel" ng-show="active" ng-transclude></div>',
+	 require: '^tabset', // Makes it a requirement for a tabset controller in the parent on this
+    scope: {
+		heading: '@' // '@' means it should be a string
+	 },
+    link: function(scope, elem, attr, tabsetCtrl) {
+		 scope.active = false;
+		 tabsetCtrl.addTab(scope);
+	 }
   }
 })
 
@@ -14,13 +20,25 @@ angular.module('app', [])
   return {
     restrict: 'E',
     transclude: true,
-    scope: { },
+    scope: {},
     templateUrl: 'tabset.html',
-    bindToController: true,
-    controllerAs: 'tabset',
+    bindToController: true, //Binds a controller to this directive
+    controllerAs: 'tabset', // Name of controller for directive
     controller: function() {
       var self = this
       self.tabs = []
+		
+		self.addTab = function(tab) {
+			if(self.tabs.length == 0) tab.active = true; // Set first tab to active
+			self.tabs.push(tab);
+		}
+		
+		self.select = function(selectedTab) {
+			self.tabs.forEach(function(tab){
+				tab.active = false;
+			});
+			selectedTab.active = true;
+		}
     }
   }
 })
