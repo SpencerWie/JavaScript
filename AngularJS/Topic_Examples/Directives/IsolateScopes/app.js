6fd43c79a8e4a2ec7099b500fff8a135e544d990
SpencerWie <spdwiecz@iupui.edu>
2016-10-16 21:49:01 -0400

    Isolate Scopes

--- AngularJS/Topic_Examples/Directives/IsolateScopes/app.js
+++ AngularJS/Topic_Examples/Directives/IsolateScopes/app.js
@@ -1,6 +1,10 @@
 // Isolate Scope is having an item "isolate" it's scope, in otherwords not share a scope with 
 // other workers.
 
+//When to use: (From AngularJS Docs)  
+// Use the scope option to create isolate scopes when making components that you want to 
+// reuse throughout your app.
+
 var app = angular.module('app', []);
 
 // Without a "scope" all of the workers will share a single 'task' variable rather than
