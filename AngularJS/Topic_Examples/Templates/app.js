38ea9ebd358a9bfa74aaae17a3705c4789e44d50
SpencerWie <spdwiecz@iupui.edu>
2016-10-29 19:33:16 -0400

    Adde AngularJS Routing

--- AngularJS/Topic_Examples/Templates/app.js
+++ AngularJS/Topic_Examples/Templates/app.js
@@ -2,12 +2,17 @@ angular.module('app', ['ngRoute'])
   .config(function($routeProvider){
     $routeProvider.when('/', {
       controller: 'TestCtrl as test',
-      templateUrl: 'test.html'
+      templateUrl: 'test.html' // On call this is palced into a $templateCache for fast reaccess
     })
     .otherwise('/');
   })
   
-  .controller('TestCtrl', function(){
+  .controller('TestCtrl', function($templateCache){
 	  this.user = {name: 'Spencer'};
+    console.log($templateCache.get('test.html')); // Gets the HTML string from thr cache
   });
 
+angular.module('app').run(function($templateCache){
+  // Direct method of placing into cache (although angular already does this automatically)
+  $templateCache.put('test.html', $templateCache.get('test.html'));
+});
