eb373db4ca06fa76cac72b5c630ec0303ff2a4a9
SpencerWie <spdwiecz@iupui.edu>
2016-10-30 00:11:24 -0400

    finnished routing example

--- AngularJS/Topic_Examples/Routing/ngView/app.js
+++ AngularJS/Topic_Examples/Routing/ngView/app.js
@@ -12,23 +12,30 @@
 // More notes: Using html5Mode with hasbang fallback
 // 
 // When using the "pertty URL mode" we need a fallback for broswers that don't support
-// .html5Mode(...), luckily html5Mode handles this and adds the # for non-compaitble browers
+// .html5Mode(...), luckily html5Mode handles this and adds the # for non-compaitble browers.
+//
+// Problems with html5Mode:
+//
+// One problem with html5Mode is that if a page is refreshed or directly linked to it will just load the normal page
+// instead of doing so through the application. Which can cause problesm with ngView, hence I would reccommend using `/#/`
+// To fix this you will need to resolve these items through server-side re-routing.
 
 var app = angular.module("app", ['ngRoute']);
 
 app.config(function($routeProvider, $locationProvider){
 
-	// If supprted we can use the `locationProvider` to remove the starting # prefix of the url files
-  $locationProvider.html5Mode({enabled: true, requireBase: false});
+	// If supprted we can use the `locationProvider` this remove the starting `#` prefix of the url files. 
+	// See section `Problems with html5Mode` above.
+  //$locationProvider.html5Mode({enabled: true, requireBase: false});
 
   $routeProvider
-  	.when("/",        // On "/" load app.html and give a controller "AppCtrl_index"
+  	.when("/",                                    // On "/" load app.html and give a controller "AppCtrl_index"
 	    {
 	      templateUrl: "app.html",
 	      controller: "AppCtrl_index",
 	      controllerAs: "ctrl"
 	    })
-  	.when('/foo',     // On "/foo" load foo.html and give a controller "AppCtrl_foo"
+  	.when('/foo',                                 // On "/foo" load foo.html and give a controller "AppCtrl_foo"
 	  	{
 	      templateUrl: "foo.html",
 	      controller: "AppCtrl_foo",
@@ -39,7 +46,13 @@ app.config(function($routeProvider, $locationProvider){
 	      templateUrl: "bar.html",
 	      controller: "AppCtrl_bar",
 	      controllerAs: "ctrl"
-	  	});
+	  	})
+  	.when('/:first/:last',{                      // We can also user variables (:varName) as well.
+	      templateUrl: "app.html",
+	      controller: "AppCtrl_params",
+	      controllerAs: "ctrl"
+  		})
+  	.otherwise({ templateUrl: "page404.html" }); // .otherwise(param) moves any unknown url links redircts to this page.
 });	
 
 
@@ -53,4 +66,8 @@ app.controller('AppCtrl_foo', function(){
 
 app.controller('AppCtrl_bar', function(){
 	this.message = "Bar!";
+});
+
+app.controller('AppCtrl_params', function($routeParams){
+	this.message = "Hello " + $routeParams.first + " " + $routeParams.last;
 });
