3971717a9f4a14d683d4c3c859c43392f3127fa8
Spencer Wieczorek <spencerwie@gmail.com>
2018-02-26 20:32:32 -0500

    Update to 0.2
    Added: Thresolding, Bluring, and sharpening functionality.

---
+++ ImageManipulator/js/imageobj.js
@@ -0,0 +1,82 @@
+var ImageObj = function(){
+  this.gui = null;
+  this.reset();
+}
+
+ImageObj.prototype.reset = function(){
+	this.greyscale = false;
+	this.brightness = 0.0;
+	this.contrast = 1.0;
+  this.threshold = -1;
+  this.sigma_blur = 0;
+  this.radius_blur = 1;
+  this.sigma_sharpen = 0;
+  this.radius_sharpen = 1;  
+	this.order = ['greyscale', 'brightness', 'contrast', 'threshold'];
+  this.update();
+  $(".panzoom").panzoom("resetZoom");
+  $(".panzoom").panzoom("resetPan");
+}
+
+ImageObj.prototype.update = function(){
+  if(imgElement.src.length == 0) return;
+	this.mat = cv.imread(imgElement);
+	this.apply_greyscale();
+	this.apply_brigtness();
+	this.apply_contrast();
+  this.apply_threshold();
+  this.apply_blur(this.sigma_blur, this.radius_blur);
+  this.apply_sharpen();
+  this.updateGUI();
+	cv.imshow('canvasOutput', this.mat);
+  $('#canvasOutput').parent('.imageContainer').height($('#imageSrc').parent('.imageContainer').height());
+}
+
+ImageObj.prototype.updateGUI = function(){
+  // Update direct elements on GUI
+  for (var i in this.gui.__controllers) {
+    this.gui.__controllers[i].updateDisplay();
+  }
+  // Update all elements inside folders
+  for (var folder in this.gui.__folders) {
+    var ele = this.gui.__folders[folder]
+    for (var i in ele.__controllers) {
+      ele.__controllers[i].updateDisplay();
+    }
+  }
+}
+
+ImageObj.prototype.apply_greyscale = function(){
+	if(this.greyscale)
+		cv.cvtColor(this.mat, this.mat, cv.COLOR_RGBA2GRAY, 0);
+}
+
+ImageObj.prototype.apply_brigtness = function(){
+	this.mat.convertTo(this.mat, -1, 1, this.brightness);
+}
+
+ImageObj.prototype.apply_contrast = function(){
+	this.mat.convertTo(this.mat, -1, this.contrast, 0);
+}
+
+ImageObj.prototype.apply_threshold = function(){
+  if(this.threshold >= 0)
+    cv.threshold(this.mat, this.mat, this.threshold, 255, cv.THRESH_BINARY);
+}
+
+ImageObj.prototype.apply_blur = function(sigma, r){
+   // kernal size must be odd, this will make any even to an odd number
+  if(r%2 == 0) r++;
+  let ksize = new cv.Size(r, r);
+  if(ksize.width > 0 && ksize.height > 0 && ksize.width % 2 == 1  && ksize.height % 2 == 1){
+      cv.GaussianBlur(this.mat, this.mat, ksize, sigma, sigma, cv.BORDER_DEFAULT);
+  }
+}
+
+//Sharpenm works by bluring then subtraction that weight as a negative to the current image  
+ImageObj.prototype.apply_sharpen = function(){
+  var originalMat = this.mat.clone();
+  this.apply_blur(this.sigma_sharpen, this.radius_sharpen);
+  cv.addWeighted(originalMat, 1.5, this.mat, -0.5, 0, this.mat);
+  
+}
---
+++ ImageManipulator/js/main.js
@@ -0,0 +1,48 @@
+var imgElement = document.getElementById('imageSrc');
+var inputElement = document.getElementById('fileInput');
+var gui;
+
+inputElement.addEventListener('change', (e) => {
+  imgElement.src = URL.createObjectURL(e.target.files[0]);
+}, false);
+
+window.onload = function(){
+	gui = new dat.GUI();
+	var img = new ImageObj();
+  
+  var greyscaleSlider = gui.add(img, 'greyscale');
+	var brightnessSlider = gui.add(img, 'brightness', -100, 100);
+	var contrastSlider = gui.add(img, 'contrast', 1, 10);
+  var thresholdtSlider = gui.add(img, 'threshold', -1, 255);
+  var resetSlider = gui.add(img, 'reset');
+  
+  greyscaleSlider.onChange(() => {img.update()});
+  brightnessSlider.onChange(() => {img.update()});
+  contrastSlider.onChange(() => {img.update()});
+  thresholdtSlider.onChange(() => {img.update()});
+  
+  var blur = gui.addFolder("Blur");
+  blur.add(img, 'sigma_blur', 0, 10).step(0.1).onChange(() => {img.update()});
+  blur.add(img, 'radius_blur', 1, 100).step(1).onChange(() => {img.update()});
+  
+  blur.open();
+  
+  var sharp = gui.addFolder("Sharpen");
+  sharp.add(img, 'sigma_sharpen', 0, 10).step(0.1).onChange(() => {img.update()});
+  sharp.add(img, 'radius_sharpen', 1, 100).step(1).onChange(() => {img.update()});
+  
+  sharp.open();
+  
+  img.gui = gui;
+  
+  setupPanZoom();
+  
+  $('#imageSrc').on('load', function(){ img.update() });
+  
+  $('body').css('padding-top', $('.navbar').outerHeight() + "px");
+  $('.dg.ac').css('top', $('.navbar').outerHeight() + "px");
+  
+  $( window ).resize(function() {
+    $('.dg.ac').css('top', $('.navbar').outerHeight() + "px");
+  });
+}
